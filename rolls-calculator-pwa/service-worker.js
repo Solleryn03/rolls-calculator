940d7d668a2868c2d1f10a8f317f7819a30d9984
Solleryn03 <elena.asenova03@gmail.com>
2025-12-18 12:45:00 +0200

    Add files via upload

---
+++ rolls-calculator-pwa/service-worker.js
@@ -0,0 +1,22 @@
+
+const CACHE = 'rolls-v2';
+const FILES = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', e => {
+  self.skipWaiting();
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+});
